12e248d5e3949be723dd0e11196d0cf99ccc6e3c
milkteapuppy <156838365+milkteapuppy@users.noreply.github.com>
2025-05-30 15:45:21 -0400

    Create dropdown menu and styling

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // script.js
+
+/* when user clicks button
+toggle between hiding and showing dropdown content */
+function showDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// close dropdown menu if user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
